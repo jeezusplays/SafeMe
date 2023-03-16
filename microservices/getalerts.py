e6c19a63a6c42e883cc59919d0c3e78b0a42ab87
Samuel Chung <41113285+samchung95@users.noreply.github.com>
2023-03-16 14:23:06 +0800

    Update getalerts.py

--- microservices/getalerts.py
+++ microservices/getalerts.py
@@ -6,43 +6,45 @@ import json
 import dateparser
 
 
-client = GDACSAPIReader()
-events = client.latest_events()
+def get_alert():
+    client = GDACSAPIReader()
+    events = client.latest_events()
 
-events_dict = events.dict()
-events = {}
-for i in events_dict['features']:
+    events_dict = events.dict()
+    events = {}
+    for i in events_dict['features']:
 
-    _from = dateparser.parse(i['properties']['fromdate'].replace('T',' '))
-    _to = dateparser.parse(i['properties']['todate'].replace('T',' '))
-    _now = datetime.now()-timedelta(hours=8)
-    _name = i['properties']['name']
-    _description = i['properties']['description']
-    _geometry = i['geometry']
-    _eventtype = i['properties']['eventtype']
-    _country = i['properties']['country']
-    _country_short = i['properties']['iso3']
-    _isHappening = _from <= _now <= _to
-    _isToday = _from.date() == _now.date()-timedelta(days=1)
+        _from = dateparser.parse(i['properties']['fromdate'].replace('T',' '))
+        _to = dateparser.parse(i['properties']['todate'].replace('T',' '))
+        _now = datetime.now()-timedelta(hours=8)
+        _name = i['properties']['name']
+        _description = i['properties']['description']
+        _geometry = i['geometry']
+        _eventtype = i['properties']['eventtype']
+        _country = i['properties']['country']
+        _country_short = i['properties']['iso3']
+        _isHappening = _from <= _now <= _to
+        _isToday = _from.date() == _now.date()-timedelta(days=1)
 
-    event = {
-        'name':_name,
-        'location':_geometry,
-        'description': _description,
-        'type':_eventtype,
-        'country':_country,
-        'country_short':_country_short,
-        'from':_from,
-        'to':_to,
-        'isHappening': _isHappening,
-        'isToday': _isToday
-    }
+        event = {
+            'name':_name,
+            'location':_geometry,
+            'description': _description,
+            'type':_eventtype,
+            'country':_country,
+            'country_short':_country_short,
+            'from':_from,
+            'to':_to,
+            'isHappening': _isHappening,
+            'isToday': _isToday
+        }
 
-    if _isToday:
-        pprint(event)
-    if events.get(_name,False):
-        if _name in events:
-            events[_name].append(event)
-    else:
-        events[_name] = [event]
-    # print(events)
+        if _isToday:
+            pprint(event)
+        if events.get(_name,False):
+            if _name in events:
+                events[_name].append(event)
+        else:
+            events[_name] = [event]
+    
+    return events
