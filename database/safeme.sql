a36a787d3d4b2379d71d6a1ac0af00b35618c40a
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-03-17 15:00:54 +0800

    Added safeme database to sql

---
+++ database/safeme.sql
@@ -0,0 +1,10 @@
+--
+-- Database: `safeme`
+--
+DROP DATABASE if exists `safeme`;
+
+CREATE DATABASE IF NOT EXISTS `safeme`; -- DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
+USE `safeme`;
+
+
+
