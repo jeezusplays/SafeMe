7f74f4c57f79f943c220633fef9dcd545deae4f6
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-03-17 15:02:00 +0800

    Added user table into sql

--- database/safeme.sql
+++ database/safeme.sql
@@ -5,6 +5,23 @@ DROP DATABASE if exists `safeme`;
 
 CREATE DATABASE IF NOT EXISTS `safeme`; -- DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
 USE `safeme`;
+DROP TABLE IF EXISTS `user`;
+
+-- Table: User
+CREATE TABLE user
+(
+  -- column_name data_type() [not null] ,
+  `userID` int not null primary key,
+  `name` varchar not null,
+  `familyID` int not null,
+  `age` int not null,
+  `country` varchar not null
+  `email` varchar not null,
+  `contact` int not null
+  -- constraint table_name_pk primary key (column_name(s)) ,
+  -- constraint table_name_fk1 foreign key (column_name) references table_name(column_name)
+);
+
 
 
 
