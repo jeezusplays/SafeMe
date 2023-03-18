825342e2894a78d7b2d712f580fcd50e720b4feb
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-03-18 15:22:24 +0800

    Added comments to user table

--- database/safeme.sql
+++ database/safeme.sql
@@ -1,5 +1,5 @@
 --
--- Database: `safeme`
+-- Database: safeme
 --
 DROP DATABASE IF EXISTS safeme;
 
@@ -10,14 +10,13 @@ DROP TABLE IF EXISTS user;
 -- Table: User
 CREATE TABLE user
 (
-  -- column_name data_type() [not null] ,
   userID int not null auto_increment,
-  name varchar(60) not null,
-  familyID int not null,
+  userName varchar(60) not null,
+  familyID int not null, -- assume always 1 (missing Family table)
   age int not null,
   country varchar(60) not null,
   email varchar(60) not null,
-  contact int not null,
+  contact int not null, -- assume no country code etc
   constraint user_pk primary key (userID)
 );
 
