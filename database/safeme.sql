9d2aa0e116762f775bef188c21eddce634d8eeec
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-03-18 13:45:51 +0800

    Updated user table

--- database/safeme.sql
+++ database/safeme.sql
@@ -11,17 +11,15 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE user
 (
   -- column_name data_type() [not null] ,
-  `userID` int not null primary key,
-  `name` varchar not null,
+  `userID` int not null auto_increment,
+  `name` varchar(60) not null,
   `familyID` int not null,
   `age` int not null,
-  `country` varchar not null
-  `email` varchar not null,
+  `country` varchar(60) not null
+  `email` varchar(60) not null,
   `contact` int not null
+  constraint user_pk primary key (`userID`)
   -- constraint table_name_pk primary key (column_name(s)) ,
   -- constraint table_name_fk1 foreign key (column_name) references table_name(column_name)
 );
-
-
-
 
