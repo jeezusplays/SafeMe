f497a7273ccf62c4320d8da4f3401b6f36a4daf1
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-03-18 14:26:42 +0800

    Update user table

--- database/safeme.sql
+++ database/safeme.sql
@@ -1,25 +1,23 @@
 --
 -- Database: `safeme`
 --
-DROP DATABASE if exists `safeme`;
+DROP DATABASE IF EXISTS safeme;
 
-CREATE DATABASE IF NOT EXISTS `safeme`; -- DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
-USE `safeme`;
-DROP TABLE IF EXISTS `user`;
+CREATE DATABASE safeme; -- DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci 
+USE safeme;
+DROP TABLE IF EXISTS user;
 
 -- Table: User
 CREATE TABLE user
 (
   -- column_name data_type() [not null] ,
-  `userID` int not null auto_increment,
-  `name` varchar(60) not null,
-  `familyID` int not null,
-  `age` int not null,
-  `country` varchar(60) not null
-  `email` varchar(60) not null,
-  `contact` int not null
-  constraint user_pk primary key (`userID`)
-  -- constraint table_name_pk primary key (column_name(s)) ,
-  -- constraint table_name_fk1 foreign key (column_name) references table_name(column_name)
+  userID int not null auto_increment,
+  name varchar(60) not null,
+  familyID int not null,
+  age int not null,
+  country varchar(60) not null,
+  email varchar(60) not null,
+  contact int not null,
+  constraint user_pk primary key (userID)
 );
 
