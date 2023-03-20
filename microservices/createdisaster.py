25bc7b664b5704573e2cf90049a16c8410bb14ec
Samuel Chung <samuelchung95@gmail.com>
2023-03-20 13:57:01 +0800

    Update createdisaster.py

--- microservices/createdisaster.py
+++ microservices/createdisaster.py
@@ -10,8 +10,6 @@ QUEUE = ''
 CONNECTION = pika.BlockingConnection(pika.ConnectionParameters(host=HOST, port=PORT))
 
 
-
-
 # Get GDAC alert
 # gdac.alert
 def alertCallback(ch, method, properties, body):
@@ -26,7 +24,7 @@ def subscribeAlert():
     channel.queue_declare(queue='hello')    
 
     # Register the callback function to consume messages from the 'hello' queue
-    channel.basic_consume(queue='hello', on_message_callback=alertHandler, auto_ack=True)
+    channel.basic_consume(queue='hello', on_message_callback=alertCallback, auto_ack=True)
 
     # Start consuming messages from the 'hello' queue
     channel.start_consuming()
