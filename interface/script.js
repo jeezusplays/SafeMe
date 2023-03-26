af647387fadb1d79230aa3a18c5755224a0fa50c
anthonyckho <anthony.ho.2021@scis.smu.edu.sg>
2023-03-26 22:07:49 +0800

    Check-in modal
    added computed properties and conditions to show the check-in modal

--- interface/script.js
+++ interface/script.js
@@ -2,3 +2,9 @@
 $(document).ready(function () {
     $('#example').DataTable();
 });
+
+//Load modal on load
+const checkInModal = new bootstrap.Modal('#checkInModal')
+window.addEventListener('DOMContentLoaded', () => {
+    checkInModal.show()
+})
