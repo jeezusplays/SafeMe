931bd8c497e4784242540ef6277512da672e23d6
anthonyckho <anthony.ho.2021@scis.smu.edu.sg>
2023-03-27 16:36:37 +0800

    Toasts for check-in modal added
    When user clicks on a button at the check-in modal, a toast will appear to show the receipt of receipt.

--- interface/script.js
+++ interface/script.js
@@ -3,20 +3,44 @@ $(document).ready(function () {
     $('#example').DataTable();
 });
 
-//Show modal on load
+//Get check-in modal buttons to show success toast in disasters near me
+const toastTrigger1 = document.getElementById('liveToastBtn1')
+const toastLiveExample1 = document.getElementById('liveToast1')
+if (toastTrigger1) {
+    toastTrigger1.addEventListener('click', () => {
+    // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    const toastBootstrap1 = new bootstrap.Toast(toastLiveExample1)
+    toastBootstrap1.show()
+    })
+}
+
+const toastTrigger2 = document.getElementById('liveToastBtn2')
+const toastLiveExample2 = document.getElementById('liveToast2')
+if (toastTrigger2) {
+    toastTrigger2.addEventListener('click', () => {
+    // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    const toastBootstrap2 = new bootstrap.Toast(toastLiveExample2)
+    toastBootstrap2.show()
+    })
+}
+
+const toastTrigger3 = document.getElementById('liveToastBtn3')
+const toastLiveExample3 = document.getElementById('liveToast3')
+if (toastTrigger3) {
+    toastTrigger3.addEventListener('click', () => {
+    // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    const toastBootstrap3 = new bootstrap.Toast(toastLiveExample3)
+    toastBootstrap3.show()
+    })
+}
+
+
+
+
+
+//Show modal on load (for some reason if i put this at the top, it will casue other scripts to fail to run)
 const checkInModal = new bootstrap.Modal('#checkInModal')
 window.addEventListener('DOMContentLoaded', () => {
     checkInModal.show()
 })
 
-//Get check-in modal buttons to show success toast
-// const toastTrigger = document.getElementById('liveToastBtn')
-// // const toastTrigger = document.getElementsByClassName('myBtn')[0]
-// const toastLiveExample = document.getElementById('liveToast')
-
-// if (toastTrigger) {
-//   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-//   toastTrigger.addEventListener('click', () => {
-//     toastBootstrap.show()
-//   })
-// }
