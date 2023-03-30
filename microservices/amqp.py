afd9ae30bec650ec0cf6f34a360761e55a07553a
Samuel Chung <41113285+schungzhihuai@users.noreply.github.com>
2023-03-30 14:25:53 +0800

    Update amqp.py

--- microservices/amqp.py
+++ microservices/amqp.py
@@ -28,6 +28,33 @@ import pika
 HOST = "localhost"  # default hostname
 PORT = 5672  # default port
 
+def check_setup():
+    # The shared connection and channel created when the module is imported may be expired,
+    # timed out, disconnected by the broker or a client;
+    # - re-establish the connection/channel is they have been closed
+    global connection, channel, hostname, port, exchangename, exchangetype
+
+    if not is_connection_open(connection):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=hostname, port=port, heartbeat=3600, blocked_connection_timeout=3600))
+    if channel.is_closed:
+        channel = connection.channel()
+        channel.exchange_declare(
+            exchange=exchangename, exchange_type=exchangetype, durable=True)
+
+def is_connection_open(connection):
+    # For a BlockingConnection in AMQP clients,
+    # when an exception happens when an action is performed,
+    # it likely indicates a broken connection.
+    # So, the code below actively calls a method in the 'connection' to check if an exception happens
+    try:
+        connection.process_data_events()
+        return True
+    except pika.exceptions.AMQPError as e:
+        print("AMQP Error:", e)
+        print("...creating a new connection.")
+        return False
+
 def create_queues(queues:list(tuple)):
     # Connect to RabbitMQ server
 
@@ -49,7 +76,6 @@ def create_queues(queues:list(tuple)):
         channel.queue_bind(queue=queue, exchange=exchange_name, routing_key=key)
     # Close the connection
     connection.close()
-
 
 if __name__ == '__main__':
     queues = [('gdacalert','gdac.alert'),('logalert','log.alert'),('logevent','log.event')]
