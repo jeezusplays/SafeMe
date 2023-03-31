63b4d1e21b4634a30901ee292c5204710693b64f
anthonyckho <anthony.ho.2021@scis.smu.edu.sg>
2023-03-31 10:09:23 +0800

    added toast functionality to volunteerPublic.html

--- interface/script.js
+++ interface/script.js
@@ -65,38 +65,38 @@ for (let report of reportToasts){
 
 
 //Adds event listener to buttons in 'Family' cards to trigger toasts upon escalation
-// const volunteerPublicToasts = [
-//     {
-//         buttonID: '1',
-//         toastID: 'a'
-//     },
-//     {
-//         buttonID: '2',
-//         toastID: 'b'
-//     },
-//     {
-//         buttonID: '3',
-//         toastID: 'c'
-//     },
-//     {
-//         buttonID: '4',
-//         toastID: 'd'
-//     },
-//     {
-//         buttonID: '5',
-//         toastID: 'e'
-//     },
-// ]
-// for (let volunteer of volunteerToasts){
-//     const toastTrigger = document.getElementById(volunteer.buttonID)
-//     const toastLiveExample = document.getElementById(volunteer.toastID)
-//     if (toastTrigger) {
-//         toastTrigger.addEventListener('click', () => {
-//             const toastBootstrap = new bootstrap.Toast(toastLiveExample)
-//             toastBootstrap.show()
-//         })
-//     }
-// }
+const volunteerPublicToasts = [
+    {
+        buttonID: '1',
+        toastID: 'a'
+    },
+    {
+        buttonID: '2',
+        toastID: 'b'
+    },
+    {
+        buttonID: '3',
+        toastID: 'c'
+    },
+    {
+        buttonID: '4',
+        toastID: 'd'
+    },
+    {
+        buttonID: '5',
+        toastID: 'e'
+    },
+]
+for (let volunteer of volunteerPublicToasts){
+    const toastTrigger = document.getElementById(volunteer.buttonID)
+    const toastLiveExample = document.getElementById(volunteer.toastID)
+    if (toastTrigger) {
+        toastTrigger.addEventListener('click', () => {
+            const toastBootstrap = new bootstrap.Toast(toastLiveExample)
+            toastBootstrap.show()
+        })
+    }
+}
 
 //Show modal on load (for some reason if i put this at the top, it will cause other scripts to fail to run)
 const checkInModal = new bootstrap.Modal('#checkInModal')
