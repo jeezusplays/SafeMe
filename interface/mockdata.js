4b3ed3695794971365977247ba55e89933cbfd72
anthonyckho <anthony.ho.2021@scis.smu.edu.sg>
2023-04-01 16:34:07 +0800

    Casualty Page table + distribution calculator

---
+++ interface/mockdata.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+      key: '1',
+      country: 'Chile',
+      disaster: 'Earthquake',
+      distance: '1.0km',
+      incidence: "Thu, 23 Feb 2023 20:20:21 GMT",
+      danger: ['Low Risk'],
+    },
+    {
+      key: '2',
+      country: 'Cuba',
+      disaster: 'Earthquake',
+      distance: '5.0km',
+      incidence: "Thu, 23 Feb 2023 10:39:01 GMT",
+      danger: ['High Risk'],
+    },
+    {
+      key: '3',
+      country: 'Turkey',
+      disaster: 'Earthquake',
+      distance: '10.0km',
+      incidence: "Thu, 23 Feb 2023 00:54:02 GMT",
+      danger: ['Moderate Risk'],
+    },
+    {
+      key: '4',
+      country: 'Korea',
+      disaster: 'Fire',
+      distance: '10.0km',
+      incidence: "Thu, 23 Feb 2023 00:54:02 GMT",
+      danger: ['Low Risk'],
+    },
+  ];
