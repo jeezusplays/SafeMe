8374222abe8da3f4934e359757f0b2b7e1c662c1
Samuel Chung <samuelchung95@gmail.com>
2023-04-03 02:58:58 +0800

    Update createdisaster.py
    Major update, to be tested

--- microservices/createdisaster.py
+++ microservices/createdisaster.py
@@ -1,29 +1,132 @@
-# Flask application to query db
-import os, sys
-import json
-
-from pprint import pprint
 from invokes import invoke_http
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from pprint import pprint
 from amqp_helper import Rabbitmq
+from math import radians, sin, cos, sqrt, atan2
 
-'''
-AMQP Setup required for sending disaster data, not operational yet until AMQP is fully configured
-#import #{amqp_setup}
-'''
-
-app = Flask(__name__)
-CORS(app)
+import json
 
 # Get GDAC alert
 # gdac.alert
+def createDisaster(alerts):
+    usersLoc = getUsersLastLoc()
+    for alert in alerts:
+        try:
+            affected_userIds = affectedUsers(usersLoc,alert)
+            disaster = createDisaster(alert)
+            affected_users = getUsersById(affected_userIds)
+            disasterId = disaster.get('disasterID',0)
+            result = addAffectedUsers(affected_users,disasterId)
+            if result.get("code",400) != 200:
+                pass
+            else:
+                return None
+        except Exception as e:
+            print(e)
+            raise e
+    pass
+
 def alertCallback(ch, method, properties, body):
     data = json.loads(body)
     pprint(data)
+    createDisaster(data)
     # print("Received message:", data)
 
     pass
+
+def distanceFrom(lat1,lon1,lat2,lon2)->float:
+    '''
+    Return distance from 2 points in km
+    '''
+    R = 6371  # radius of the earth in km
+
+    # convert latitudes and longitudes from degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    # calculate the difference between the latitudes and longitudes
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    # apply the Haversine formula to calculate the distance
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    distance = R * c
+
+    return distance
+    
+def getUsersLastLoc():
+    try:
+        result = invoke_http("http://0.0.0.0:5001/location/latest", method="GET")
+        if result.get("code",400) == 200:
+            return result
+        else:
+            return None
+    except Exception as e:
+        print(e)
+        raise e
+
+def getUsersById(userIds):
+    users = []
+    for ids in userIds:
+        try:
+            result = invoke_http(f"http://0.0.0.0:5002/user/{ids}",method="GET")
+            if result.get("code",400) == 200:
+                users.append(result)
+            else:
+                raise Exception('Error getting users by user id createdisaster.py')
+        except Exception as e:
+            print(e)
+            raise e
+    return users
+
+def addAffectedUsers(user, disasterId):
+    affectedUser = {
+        "disasterID":disasterId,
+        "userID":user["userID"],
+        "userName":user["userName"],
+        "status": "Pending",
+        "contact":user["contact"]
+    }
+    
+    try:
+        result = invoke_http('http://0.0.0.0:5002/affecteduser', method='POST', json=affectedUser)
+        if result.get("code",400) == 200:
+            return result
+        else:
+            return Exception('error adding affected user createdisaster.py')
+    except Exception as e:
+        print(e)
+
+def createDisaster(alert):
+    data = alert
+    try:
+        result = invoke_http("http://localhost:5002/disaster/new", method="POST", json=data)
+
+        if result.get("code",400) == 200:
+            return result
+        else:
+           raise SystemError('Unable to create disaster')
+        
+    except Exception as e:
+        raise e
+    
+def affectedUsers(usersLoc,alert:dict):
+    location = alert.get('location',{'coordinates':[0,0],'type':'point'})
+    coordinates = location.get('coordinates')
+
+    userIds = []
+
+    for user in usersLoc:
+        userID = user['userID']
+        country = user['country']
+        city = user['city']
+        lat = user['lat']
+        long = user['long']
+        timestamp = user['timestamp']
+
+        if distanceFrom(lat,long,coordinates[0],coordinates[1]) <= 4:
+            userIds.append(userID)
+        
+    return userIds
 
 def main():
     rabbitmq = Rabbitmq()
@@ -34,48 +137,13 @@ def main():
 
 # Get all user latest location - send request
 # [GET] /location/latest
-@app.route("/location/latest", methods=["GET"])
-def get_all_users_latest_location():
-    print("Getting all users latest location...")
-    # Get all users latest location
-    response = invoke_http("http://localhost:5001/location/latest", method="GET")
-    print("Response: ", response)
-    return response
 
 # Create disaster - send request
 # [POST] /disaster/new
-@app.route("/disaster/new", methods=["POST"])
-def create_disaster():
-    if request.is_json:
-        try:
-            print("Creating disaster...")
-            print("----- Invoking disaster microservice to get user data -----")
-            data = request.get_json()
-            # Create disaster
-            response = invoke_http("http://localhost:5002/disaster/new", method="POST", json=data)
-            print("Result from disaster microservice: ", response)
-            return response
-        
-        except Exception as e:
-            # Unexpected error in code
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
-            print(ex_str)
-            return jsonify({
-                "code": 500,
-                "message": "createdisaster.py internal error: " + ex_str
-            }), 500 
-    
-    return jsonify({
-        "code": 400,
-        "message": "createdisaster.py bad request (Invalid JSON input): " + str(request.get_json())
-    }), 400
 
 
 # Execute this program if it is run as a main script
 if __name__ == "__main__":
-    print("This is flask " + os.path.basename(__file__) + " for querying user status...")
-    app.run(host="0.0.0.0", port=5200, debug=True)
+    main()
 
 
