5a24862161650d66068572e7e4c49aa3924be8a8
Samuel Chung <samuelchung95@gmail.com>
2023-04-04 18:39:23 +0800

    Update websocket_test.py

--- microservices/websocket_test.py
+++ microservices/websocket_test.py
@@ -1,23 +1,18 @@
 from websocket import *
-
-def on_message(ws, message):
-    print(message)
-
-def on_error(ws, error):
-    print(error)
-
-def on_close(ws, namespace, sid):
-    print("### closed ###")
-
-def on_open(ws):
-    print("### connected ###")
-    ws.send("Hello, server!")
+from time import sleep
+import json
 
 if __name__ == "__main__":
     enableTrace(True)
-    ws = WebSocketApp("ws://localhost:5000/ws",
-                                on_message = on_message,
-                                on_error = on_error,
-                                on_close = on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+    ws = WebSocket()
+    ws.connect("ws://localhost:5000/ws")
+    test1 ={
+        "message":"getUserAlert",
+        "userID":1
+    }
+    count = 10
+    while count>0:
+        ws.send(json.dumps(test1))
+        sleep(2)
+        count -= 1
+    ws.close()
