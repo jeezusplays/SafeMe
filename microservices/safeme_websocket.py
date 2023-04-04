ee4f278aaf0ade8fd1c8135d1b50a51fe3175121
Samuel Chung <samuelchung95@gmail.com>
2023-04-04 20:45:48 +0800

    update

--- microservices/safeme_websocket.py
+++ microservices/safeme_websocket.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_sock import Sock
 from amqp_helper import Rabbitmq
 from time import sleep
-
-
 import json
 
 app = Flask(__name__)
@@ -15,59 +13,61 @@ class MessageCentre:
             'user':{},
             'family':{}
         }
-        pass
     def _add(self,msg,type,id):
         if type == 'user' or type == 'family':
-            # If user message doesnt exist, add in a new list, else add to list
             self.messages[type][id] = self.messages[type].get(id,[])+[msg]
+            print(self.messages)
     def getUserAlert(self,userID):
+        print(self.messages['user'])
         return self.messages['user'].get(userID,[])
     def getFamilyAlert(self,familyID):
         return self.messages['family'].get(familyID,[])
 
-def callback(ch, method, properties, body):
+def callback_user_alert(ch, method, properties, body):
+    routing_key = method.routing_key
     data = json.loads(body)
+    id = routing_key.split('.')[1]
+    type = "user"
 
-    print(data)
+    print(id)
+    print(type)
 
-    type = data.get('type',None)
-    id = data.get('id',None)
-    msg = data.get('msg',None)
-    messageCentre._add(msg=msg,type=type,id=id)
-
-
+    message_centre._add(msg=data,type=type,id=id)
+        
 @sock.route('/ws')
 def ws(ws):
     while True:
         txt = ws.receive()
         print(txt)
         try:
-            data = json.loads(txt)
-            message_type = data.get('message',"Error")
-            if message_type == "getUserAlert":
-                id = data.get('userID')
-                if id is not None and messageCentre:
-                    msg = messageCentre.getUserAlert(id)
-                    ws.send(json.dumps({'code': 200, 'data': msg}))
-            elif message_type == "getFamilyAlert":
-                id = data.get('familyID')
-                if id is not None and messageCentre:
-                    msg = messageCentre.getUserAlert(id)
-                    ws.send(json.dumps({'code': 200, 'data': msg}))
-
+            with app.app_context():
+                data = json.loads(txt)
+                message_type = data.get('message', "Error")
+                if message_type == "getUserAlert":
+                    id = data.get('userID')
+                    print(message_centre)
+                    if id is not None and message_centre:
+                        print(message_centre.messages)
+                        msg = message_centre.getUserAlert(str(id))
+                        ws.send(json.dumps({'code': 200, 'data': msg}))
+                elif message_type == "getFamilyAlert":
+                    id = data.get('familyID')
+                    if id is not None and message_centre:
+                        msg = message_centre.getUserAlert(id)
+                        ws.send(json.dumps({'code': 200, 'data': msg}))
         except Exception as e:
             print(e)
 
-
 if __name__ == '__main__':
-    global messageCentre
-    messageCentre = MessageCentre()
     rabbitmq = Rabbitmq()
-    print("subscribing to user_1 and family_1")
-    rabbitmq.subscribe('user_1')
-    rabbitmq.subscribe('family_1')
-    # rabbitmq.subscribe('family_1')
-    app.run(debug=True)
-    rabbitmq.unsubscribe()
-    
+    message_centre = MessageCentre()
 
+    print("subscribing to user_1 and family_1")
+    rabbitmq.subscribe('user_1', callback_user_alert)
+    rabbitmq.subscribe('family_1', callback_user_alert)
+    try:
+        app.run(host='0.0.0.0', port=9000)
+    except:
+        pass
+    finally:
+        rabbitmq.unsubscribe()
--- microservices/websocket_test.py
+++ microservices/websocket_test.py
@@ -5,7 +5,7 @@ import json
 if __name__ == "__main__":
     enableTrace(True)
     ws = WebSocket()
-    ws.connect("ws://localhost:5000/ws")
+    ws.connect("ws://localhost:9000/ws")
     test1 ={
         "message":"getUserAlert",
         "userID":1
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     count = 10
     while count>0:
         ws.send(json.dumps(test1))
+        print(ws.recv())
         sleep(2)
         count -= 1
     ws.close()
