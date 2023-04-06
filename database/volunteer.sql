47b2f36a11a1f9f850d3e4c71d45d7a5da4b82a0
hx240 <hx24000@gmail.com>
2023-04-07 02:23:00 +0800

    Update volunteer.sql

--- database/volunteer.sql
+++ database/volunteer.sql
@@ -21,20 +21,6 @@ CREATE TABLE volunteer
 INSERT INTO volunteer (volunteerEventID, userID, userName, contact, `timestamp`)
 VALUES
     (1, 1, 'John', 8888888, '2021-03-15'),
-    (1, 2, 'Jane', 8888888, '2021-03-15'),
-    (1, 3, 'Bob', 8888888, '2021-03-15'),
-    (2, 4, 'Alice', 8888888, '2021-04-20'),
-    (2, 5, 'Michael', 8888888, '2021-04-20'),
-    (2, 6, 'Maria', 8888888, '2021-04-20'),
-    (3, 7, 'Juan', 8888888, '2021-05-10'),
-    (3, 8, 'Luis', 8888888, '2021-05-10'),
-    (4, 9, 'Sophie', 8888888, '2021-06-05'),
-    (5, 10, 'Mohd', 8888888, '2021-07-01');
-
--- Insert 10 rows into the volunteer table, referencing the volunteerEvent table
-INSERT INTO volunteer (volunteerEventID, userID, userName, contact, `timestamp`)
-VALUES
-    (1, 1, 'John', 8888888, '2021-03-15'),
     (2, 2, 'Jane', 8888888, '2021-03-15'),
     (3, 3, 'Bob', 8888888, '2021-03-15'),
     (4, 4, 'Alice', 8888888, '2021-04-20'),
